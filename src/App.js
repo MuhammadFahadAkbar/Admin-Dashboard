248364c9d425e726640e1a595538795fd45bd3a7
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2023-03-11 13:55:32 +0500

    Added Team Page using DataGrid

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard";
-// import Team from "./pages/global/team";
+import Team from "./pages/team";
 // import Contacts from "./pages/global/contacts";
 // import Invoices from "./pages/global/invoices";
 // import Form from "./pages/global/form";
@@ -28,7 +28,7 @@ const App = () => {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} /> */}
+              <Route path="/team" element={<Team />} />
               {/* <Route path="/contacts" element={<Contacts />} /> */}
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
